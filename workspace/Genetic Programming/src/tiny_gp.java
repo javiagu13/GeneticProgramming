e9c7dad7cf32a298e65b275958a700664786f787
javiagu13 <javiagu13@gmail.com>
2020-08-18 18:31:41 +0200

    JaviGP added, population created successfully

--- workspace/Genetic Programming/src/tiny_gp.java
+++ workspace/Genetic Programming/src/tiny_gp.java
@@ -382,7 +382,7 @@ public class tiny_gp {
     }
 
     public static void main(String[] args) {
-        String fname = "problem.dat";
+        String fname = "C:/Users/Javi/Desktop/problem.dat";
         long s = -1;
 
         if ( args.length == 2 ) {
